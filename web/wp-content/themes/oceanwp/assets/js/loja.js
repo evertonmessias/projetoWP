37fd4b9a407ab256b7d26163e0de47b7de107115
evertonmessias <everton.messias@gmail.com>
2020-04-19 14:36:56 -0300

    19-04-2020_14:36:56

--- web/wp-content/themes/oceanwp/assets/js/loja.js
+++ web/wp-content/themes/oceanwp/assets/js/loja.js
@@ -1,16 +1,5 @@
 window.onload = function(){
 	
-	/*
-	var url = window.location.href;
-	var teste = /\/produto/.test(url);
-	if(teste){
-		$("#top-bar").css({"display":"none"});
-	}else{
-		$("#top-bar").css({"display":"block"});
-	}
-	*/
-	
-	
     $(window).scroll(function () {		
 		if ($(this).scrollTop() > 40) {$('#site-header').addClass("menu-fixo");}
 		else {$('#site-header').removeClass("menu-fixo");}		
